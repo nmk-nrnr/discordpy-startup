b69637778765218d48779b30bbf640276b7329b4
nmk-nrnr <71581186+nmk-nrnr@users.noreply.github.com>
2020-09-20 19:04:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,20 @@ import json
 import copy
 from datetime import datetime, timedelta, timezone
 
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
+
+bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+    bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
 Hololive = {
     "UCa5JOPRFXMbYz4pBUxAuBaA": [
         "まこふるちゃんねる",
